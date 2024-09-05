0fed60a17ec1c24ea86b4254718a76c34f3c1575
JanisCatalina10 <janiscatalinaa@gmail.com>
2024-09-04 20:36:43 -0400

    script.js color green

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,25 @@
-function pintar(){
+/* CODIGO ORIGINAL:
+ * function pintar(){
     ele.style.backgroundColor = 'yellow'
     }
     const ele = document.getElementById("ele1")
-    ele.addEventListener("click", pintar);
+    ele.addEventListener("click", pintar); */
+
+
+    /*CODIGO MODIFICADO 2.2
+    const ele = document.getElementById("ele1");
+    function pintar(ele) {
+        ele.style.backgroundColor = 'yellow';  
+    }
+    ele.addEventListener("click", () => pintar(ele));*/
     
+
+    /*CODIGO MODIFICADO 2.3 */
+    const ele = document.getElementById("ele1");
+    function pintar(ele, color) {
+        ele.style.backgroundColor = color; 
+    }
+    ele.addEventListener("click", () => {
+        pintar(ele, 'green');  
+    }); 
+        
